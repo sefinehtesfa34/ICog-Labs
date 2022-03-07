c03cafaa366d77d33cb3a054f75984d06e82c215
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-03-07 02:03:23 -0800

    SVD latent factor preferences

--- recommendation-system/item_based_recommeder_system.py
+++ recommendation-system/item_based_recommeder_system.py
@@ -77,6 +77,63 @@ data=Dataset.load_from_df(ratings_flrd_df[["userId","movieId","rating"]],reader=
 trainset=data.build_full_trainset()
 testset=trainset.build_anti_testset()
 
+# Number of Factors and RMSE
+# For the demonstrative purpose, let's examine the effect of number of latent factors k on the model\'s 
+# performance. Specifically, it is possible to visually observe the effect of 
+# multiple factors on error measurement. 
+# As in supervised machine learning, 
+# cross_validate computes the error rate for each fold. 
+# The following function computes the average of RMSE given by the five folds and 
+# append the empty list rmse_svd. Consequently, the list contains 100 measures of min RMSE 
+# given 100 consecutive values of k in each test set, and by five folds in every iteration.
+
+
+def rmse_vs_factors(algorithm, data):
+  """Returns: rmse_algorithm i.e. a list of mean RMSE of CV = 5 in cross_validate() for each  factor k in range(1, 101, 1)
+  100 values 
+  Arg:  i.) algorithm = Matrix factoization algorithm, e.g SVD/NMF/PMF, ii.)  data = surprise.dataset.DatasetAutoFolds
+  """
+  
+  rmse_algorithm = []
+  
+  for k in range(1, 101, 1):
+    algo = algorithm(n_factors = k)
+    
+    #["test_rmse"] is a numpy array with min accuracy value for each testset
+    loss_fce = cross_validate(algo, data, measures=['RMSE'], cv=5, verbose=False)["test_rmse"].mean() 
+    rmse_algorithm.append(loss_fce)
+  
+  return rmse_algorithm
+rmse_svd = rmse_vs_factors(SVD,data)
+# To replicate the plot of performance for each subsequent model, 
+# the following chunk defines the function plot_rmse() with two arguments 
+# where rmse is a list of float values and algorithm is an instantiated matrix factorization model. 
+# The function returns a plot with two line subplots that display performance vs. numbers of factors. 
+# The second subplot only zooms in and marks k with the best performance, i.e. the minimum RMSE.
+
+def plot_rmse(rmse, algorithm):
+  """Returns: sub plots (2x1) of rmse against number of factors. 
+     Vertical line in the second subplot identifies the arg for minimum RMSE
+    
+     Arg: i.) rmse = list of mean RMSE returned by rmse_vs_factors(), ii.) algorithm = STRING! of algo 
+  """
+  
+  plt.figure(num=None, figsize=(11, 5), dpi=80, facecolor='w', edgecolor='k')
+
+  plt.subplot(2,1,1)
+  plt.plot(rmse)
+  plt.xlim(0,100)
+  plt.title("{0} Performance: RMSE Against Number of Factors".format(algorithm), size = 20 )
+  plt.ylabel("Mean RMSE (cv=5)")
+
+  plt.subplot(2,1,2)
+  plt.plot(rmse)
+  plt.xlim(0,50)
+  plt.xticks(np.arange(0, 52, step=2))
+
+  plt.xlabel("{0}(n_factor = k)".format(algorithm))
+  plt.ylabel("Mean RMSE (cv=5)")
+  plt.axvline(np.argmin(rmse), color = "r")
 
 
 
