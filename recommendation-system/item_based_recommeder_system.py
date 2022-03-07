3b27efb424c356e67493d68ee0589948ebb2a41a
Sefineh Tesfa <sefinehtesfa34@gmail.com>
2022-03-07 00:40:17 -0800

    Data preprocessing

--- recommendation-system/item_based_recommeder_system.py
+++ recommendation-system/item_based_recommeder_system.py
@@ -44,15 +44,30 @@ movies_df=pd.read_csv(zipfile.open('ml-latest-small/movies.csv'))
 # print(ratings_df.head())
 # print(ratings_df.info())
 # print(ratings_df.describe())
-print(movies_df.head())
+# print(movies_df.head())
 
 #Note that movies_df contains only movieId and genres variables which store even multiple genres 
 # separated by the vertical bar in one cell.
 # Data preprocessing
+# Filtering Data Set
+# Firstly, it is essential to filter out movies and users with low exposure to 
+# remove some of the noise from outliers. According to the official 
+# MovieLens documentation, all selected users have rated at least 20 movies in the data set. 
+# However, the following code filters out the movies and 
+# users based on an arbitrary threshold 
+# and creates a new data frame ratings_flrd_df. 
+# Moreover, the chunk also prints the value of deleted movies 
+# with new and old dimensions.
+
+min_movie_ratings = 2 #a movie has was rated at least 
+min_user_ratings =  5 #a user rated movies at least
+
+
+ratings_flrd_df = ratings_df.groupby("movieId").filter(lambda x: x['movieId'].count() >= min_movie_ratings)
+ratings_flrd_df = ratings_flrd_df.groupby("userId").filter(lambda x: x['userId'].count() >= min_user_ratings)
 
 
 
-
-
-
-
+print("{0} movies deleted; all movies are now rated at least: {1} times. Old dimensions: {2}; New dimensions: {3}"\
+.format(len(ratings_df.movieId.value_counts()) - len(ratings_flrd_df.movieId.value_counts())\
+        ,min_movie_ratings,ratings_df.shape, ratings_flrd_df.shape ))
